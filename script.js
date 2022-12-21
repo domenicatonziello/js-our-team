8629871f4989c9ca7325518c98a3475c01db96ea
domenicatonziello <domenica.tonziello10@gmail.com>
2022-12-21 15:38:37 +0100

    milestone 0

--- script.js
+++ script.js
@@ -17,3 +17,42 @@ Trasformare la stringa foto in una immagine effettiva
 *BONUS 2:
 Organizzare i singoli membri in card/schede e rendere la pagina gradevole (potete usare lo screen in allegato come spunto
 */
+
+// col --> card --> img (pb-10) / h3 / p (pb-10)
+
+// prendo elementi dal DOM
+const row = document.querySelector('.row');
+
+// creo array del team
+const team = [
+    {
+        name: 'Wayne Barnett',
+        role: 'Founder & CEO',
+        image: 'wayne-barnett-founder-ceo.jpg'
+    },
+    {
+        name: 'Angela Caroll',
+        role: 'Chief Editor',
+        image: 'angela-caroll-chief-editor.jpg'
+    },
+    {
+        name: 'Walter Gordon',
+        role: 'Office Manager',
+        image: 'walter-gordon-office-manager.jpg'
+    },
+    {
+        name: 'Angela Lopez',
+        role: 'Social Media Manager',
+        image: 'angela-lopez-social-media-manager.jpg'
+    },
+    {
+        name: 'Scott Estrada',
+        role: 'Developer',
+        image: 'scott-estrada-developer.jpg'
+    },
+    {
+        name: 'Barbara Ramos',
+        role: 'Graphic Designer',
+        image: 'barbara-ramos-graphic-designer.jpg'
+    }
+]
